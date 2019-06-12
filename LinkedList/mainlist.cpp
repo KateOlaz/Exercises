209fed66aa0e3aa006c392e12fb289617b02b140
KateOlaz <49486445+KateOlaz@users.noreply.github.com>
2019-06-12 11:16:31 -0500

    Update mainlist.cpp

--- LinkedList/mainlist.cpp
+++ LinkedList/mainlist.cpp
@@ -12,6 +12,7 @@ int main()
     int ele;
     int dim;
     int pos;
+    int n;
  
     cout << "Ingresa la dimensión de la lista: " << endl;
     cin >> dim;
@@ -22,12 +23,23 @@ int main()
     list1.lista(dim);
     list1.imprimir();
 
+    cout<<"Nuevo elemento a insertar: "<<endl;
+    cin>>n;
+    list1.insertar_final(n);
+    list1.imprimir();
+
     cout<< "que elemento desea buscar: "<< endl;
     cin>>pos;
     list1.buscar(pos);
+    
     
 
     list1.eliminar_todo();
 
     return 0;
 }
+
+
+
+
+//Material de Guía: https://ronnyml.wordpress.com/2009/07/04/listas-enlazadas-clase-lista-en-c/
