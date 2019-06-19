bc38753259bfcb79decb42f83cb498aba1deff36
KateOlaz <49486445+KateOlaz@users.noreply.github.com>
2019-06-19 11:11:00 -0500

    Update list.cpp

--- Stack/list.cpp
+++ Stack/list.cpp
@@ -1,155 +1,78 @@
 #include "list.h"
-#include "iteradorlist.h"
 
 template<typename T>
-Node<T> LinkedList<T>::inicio()
+Node<T>* Pila<T>::CrearPila()
 {
-    return Node<T>(head);
+    return head=NULL;
 }
 
 template<typename T>
-Node<T> LinkedList<T>::end()
+bool Pila<T>::pilaVacia()
 {
-    Node<T> *temp = head;
-	while (temp != NULL) {
-		temp = temp->next;
-	}
-	return temp;
+    if(head==NULL);
 }
 
+//TOP
 template<typename T>
-LinkedList<T>::LinkedList()
+T Pila<T>::TopePila()
 {
-    size = 0;
-    head = NULL;
+    return head->dato;
 }
 
-//insertar al inicio
+// PUSH
 template<typename T>
-void LinkedList<T>::insertar_inicio(T valor)
+Node<T>* Pila<T>::InsertarPila(T valor)
 {
     Node<T> *newNode = new Node<T>(valor);
-    Node<T> *temp = head;
- 
-    if (!head) 
+    if(newNode!=NULL)
     {
-        head = newNode;
-    } 
-    else 
+        newNode->dato=valor;
+        newNode->next=head;
+        head=newNode;
+    }
+    return head;
+}
+// POP
+template<typename T>
+Node<T>* Pila<T>::EliminarPila()
+{
+    Node<T> *NodeAux;
+    if(!pilaVacia())
     {
-        newNode->next = head;
-        head = newNode;
+        NodeAux=head;
+        head=head->next;
+        delete(NodeAux);
     }
-    size++;
+    return head;
 }
 
-// Insertar al final
+// Imprimir Pila
 template<typename T>
-void LinkedList<T>::insertar_final(T valor)
+void Pila<T>::imprimirPila()
 {
-    Node<T> *newNode = new Node<T> (valor);
-    Node<T> *temp = head;
- 
-    if (!head) {
-        head = newNode;
-    } else {
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-    }
-    size++;
-}
-// Llenar la Lista por teclado
-template<typename T>
-void LinkedList<T>::lista(int dim)
-{
-    T ele;
-    for (int i = 0; i < dim; i++) {
-        cout << "Ingresa el elemento " << i + 1 << endl;
-        cin >> ele;
-        insertar_final(ele);
-    }
-}
-
-// Buscar el dato de un nodo
-template<typename T>
-void LinkedList<T>::buscar(T valor)
-{
-    Node<T> *temp = head;
-    int cont = 1;
-    int cont2 = 0;
- 
-    while (temp) {
-        if (temp->dato == valor) {
-            cout << "El dato se encuentra en la posición: " << cont << endl;
-            cont2++;
-        }
-        temp = temp->next;
-        cont++;
-    }
- 
-    if (cont2 == 0) {
-        cout << "No existe el dato " << endl;
-    }
-    cout << endl << endl;
-}
-// Imprimir la Lista
-template<typename T>
-void LinkedList<T>::imprimir()
-{
-    Node<T> *temp = head;
-    if (!head) {
-        cout << "La Lista está vacía " << endl;
-    } else {
-        while (temp) {
-            temp->imprimir();
-            if (!temp->next);
-                temp = temp->next;
-        }
-  }
-  cout << endl << endl;
-}
-
-// Eliminar por data del nodo
-template<typename T>
-void LinkedList<T>::eliminar(T valor)
-{
-    Node<T> *temp = head;
-    Node<T> *temp1 = head->next;
- 
-    int cont = 0;
- 
-    if (head->data == valor) 
+    Node<T> *NodeAux;
+    NodeAux=head;
+    cout<<endl<<"Contenido de la pila"<<endl;
+    while(NodeAux!=NULL)
     {
-        head = temp->next;
-    } 
-    else {
-        while (temp1) {
-            if (temp1->data == valor) {
-                Node<T> *aux_node = temp1;
-                temp->next = temp1->next;
-                delete aux_node;
-                cont++;
-                size--;
-            }
-            temp = temp->next;
-            temp1 = temp1->next;
-        }
+        cout<<NodeAux->dato;
+        NodeAux=NodeAux->next;
     }
- 
-    if (cont == 0) {
-        cout << "No existe el dato " << endl;
-    }
+    cout<<" ->" <<endl;
 }
 
-// Eliminar todos los nodos
 template<typename T>
-void LinkedList<T>::eliminar_todo()
+Node<T>* Pila<T>::EliminarTodo()
 {
-    head->borrar_todo();
-    head = 0;
+    while(head!=NULL)
+    {
+        Node<T> *NodeAux;
+        NodeAux=head;
+        head=head->next;
+        delete(NodeAux);
+    }
+    cout<<"Eliminación exitosa"<<endl;
 }
 
 template<typename T>
-LinkedList<T>::~LinkedList() {}
+Pila<T>::~Pila() {}
